b46ad084c14eb50fdabd6887ffa4fddb12a71272
FedericoPeters <federicomusica19@gmail.com>
2026-06-05 19:46:41 -0300

    Add files via upload

---
+++ ejercicio 6.py
@@ -0,0 +1,11 @@
+
+var_1 = 8
+var_2 = 12
+var_3 = 4
+
+if var_1 > var_2 and var_1 > var_3:
+    print("El mayor es var_1")
+elif var_2 > var_1 and var_2 > var_3:
+    print("El mayor es var_2")
+elif var_3 > var_1 and var_3 > var_2:
+    print("El mayor es var_3")  
