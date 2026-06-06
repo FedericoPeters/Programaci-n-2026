a5d770b728083ce96cf1aff49d033aadb9b137b0
FedericoPeters <federicomusica19@gmail.com>
2026-06-05 21:06:16 -0300

    Add files via upload

---
+++ ejercicio 7.py
@@ -0,0 +1,20 @@
+lista_productos = ["Papas fritas", "Vaso de coca cola", "Helado de vainilla", "Hamburguesa doble con cheddar", "Hamburguesa doble "]
+lista_precios = [4000, 2800, 3600, 9000, 8000]
+
+cantidad = int(input("¿Cuántos productos desea ingresar?: "))
+
+for i in range(cantidad):
+    producto = input(f"Ingrese el nombre del producto {i+1}: ")
+    precio = float(input(f"Ingrese el precio de '{producto}': $"))
+
+    lista_productos.append(producto)
+    lista_precios.append(precio)
+    
+for i in range(len(lista_productos)):
+    print(f" {lista_productos[i]:.<40} - ${lista_precios[i]}")
+
+total = sum(lista_precios)
+promedio = total / len(lista_precios)
+
+print (f"El total es: ${total}")
+print (f"El valor promedio por producto es: ${promedio}")
